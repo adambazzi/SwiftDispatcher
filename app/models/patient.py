3fa1af2947d8a0adf10962da18ac70e07fcb3dc2
Adam Bazzi <adambazzi14@gmail.com>
2023-05-23 10:12:38 -0500

    several models created

---
+++ app/models/patient.py
@@ -0,0 +1,39 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Patient(db.Model):
+    """
+    Patient model representing patients in the application.
+    """
+    __tablename__ = 'patients'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id'), ondelete='CASCADE'), nullable=False)
+
+    specialization = db.Column(db.String(100), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    weight = db.Column(db.Integer, nullable=False)
+    is_smoker = db.Column(db.Boolean, nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    gender = db.Column(db.String(100), nullable=False)
+    race = db.Column(db.String(100), nullable=False)
+    insurance_name = db.Column(db.String(100), nullable=False)
+    policy_number = db.Column(db.String(100), nullable=False)
+    group_number = db.Column(db.String(100), nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
+
+    # Define Relationships
+    # Define a one-to-one relationship with Users
+    user = db.relationship('User', uselist=False, back_populates='patient', cascade="delete")
+    # Define one-to-many relationship with appointments table
+    appointments = db.relationship('Appointment', back_populates='patient')
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.schema import CheckConstraint
 from flask_login import UserMixin
 
 
@@ -10,24 +11,57 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
+    username = db.Column(db.String(40), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(255), nullable=False, unique=True, index=True)
+    phone = db.Column(db.String(255), nullable=False, unique=True)
+    address = db.Column(db.String(150), nullable=False)
+    city = db.Column(db.String(150), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
+    lng = db.Column(db.Float,nullable=False)
+    lat = db.Column(db.Float,nullable=False)
+    first_name = db.Column(db.String(150), nullable=False)
+    last_name = db.Column(db.String(150), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=False)
     hashed_password = db.Column(db.String(255), nullable=False)
+
+
+
+    # Relationship field
+    # Define a one-to-one relationship with doctor
+    doctor = db.relationship('Doctor', back_populates='user', uselist=False)
+    # Define a one-to-one relationship with patient
+    patient = db.relationship('Patient', back_populates='user', uselist=False)
+
+
+    # Check constraints for basic email and phone validation
+    __table_args__ = (
+        CheckConstraint('email ~* \'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$\'', name='email_check'),
+        CheckConstraint('phone ~* \'^\+\d{1,3}\s?\d{1,14}(\s?\d{1,13})?\'', name='phone_check'),
+    )
 
     @property
     def password(self):
-        return self.hashed_password
+        raise AttributeError("Password attribute is not readable.")
 
     @password.setter
     def password(self, password):
+        # Password complexity check
+        pattern = re.compile(
+            r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+        )
+        if not pattern.match(password):
+            raise ValueError(
+                "Password must contain at least 8 characters, including an uppercase letter, "
+                "lowercase letter, a digit and a special character (@$!%*?&)."
+            )
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.hashed_password, password)
 
     def to_dict(self):
         return {
             'id': self.id,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
         }
