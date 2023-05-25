0f78bd87e1438069d9ebc12f5a4890ec8f818b1f
Adam Bazzi <adambazzi14@gmail.com>
2023-05-25 10:46:53 -0500

    added job documents model

---
+++ app/models/job_documents.py
@@ -0,0 +1,30 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class Job_Document(db.Model):
+    """
+    Job_Document model representing Job Documents in the application.
+    """
+    __tablename__ = 'job_documents'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    job_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('jobs.id')), nullable=False)
+
+    category = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String, nullable=False)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+        nullable=False
+    )
+
+    # Define Relationships
+    # Define a many-to-one relationship with Jobs
+    job = db.relationship('Job', backref='job_documents')
