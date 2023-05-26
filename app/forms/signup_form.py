b909f605181870f234869ac0d3a0442097fdea81
Adam Bazzi <adambazzi14@gmail.com>
2023-05-26 12:32:20 -0500

    added signup form in flask

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired, Email, ValidationError, Length, Regexp
 from app.models import User
 
 
@@ -21,7 +21,18 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
-    username = StringField(
-        'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    username = StringField('username', validators=[DataRequired(), username_exists, Length(max=40)])
+    email = StringField('email', validators=[DataRequired(), Email(), user_exists])
+    password = StringField(
+    'password',
+    validators=[
+        DataRequired(),
+        Regexp(
+            r"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            message="Password must contain at least 8 characters, including an uppercase letter, lowercase letter, a digit and a special character (@$!%*?&)."
+            )
+        ]
+    )
+    first_name = StringField('first_name', validators=[DataRequired(), Length(max=150)])
+    last_name = StringField('last_name', validators=[DataRequired(), Length(max=150)])
+    phone = StringField('phone', validators=[DataRequired(), Length(max=255)])  # You might want to add phone number validation here
